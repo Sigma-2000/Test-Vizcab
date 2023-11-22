7e25f145559c5f74319f6f4f80a612f73c3ec411
Amélie Guyot <amelieguyot7@gmail.com>
2023-11-22 13:16:13 +0100

    Fix-tests-buildings-memoryRouter

--- frontend/src/components/Buildings.test.jsx
+++ frontend/src/components/Buildings.test.jsx
@@ -2,6 +2,7 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import { test, expect } from 'vitest';
+import { MemoryRouter } from 'react-router-dom'; // ou BrowserRouter
 
 import Buildings from './Buildings';
 
@@ -9,7 +10,7 @@ test('renders the Buildings component with data', () => {
 
     const {
         getByText, queryAllByText, queryAllByTestId,
-    } = render(<Buildings />);
+    } = render(<MemoryRouter><Buildings /></MemoryRouter>);
 
     // Check that the correct number of buildings renders.
     expect(queryAllByTestId('buildings')).toHaveLength(8);
